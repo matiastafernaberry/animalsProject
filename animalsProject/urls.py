cd4652b4a61a2f51e542878c9baea099937ddc96
tafernaberry <gonzalo.tafernaberry@gmail.com>
2020-10-29 17:42:39 -0300

    second commit

--- animalsProject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from animalsProject.views import DogRandomView, CatRandomView
-
-urlpatterns = [
-    path('dog/random', DogRandomView.as_view(), name="home"),
-    path('cat/random', CatRandomView.as_view(), name="home"),
-]
