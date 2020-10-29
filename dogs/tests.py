15c80e7b71d430f60292a6d57f653123fc8ca064
tafernaberry <gonzalo.tafernaberry@gmail.com>
2020-10-29 19:11:35 -0300

    fixed static file repeat and add unitest

--- dogs/tests.py
+++ dogs/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser
+from django.test import TestCase, RequestFactory
 
-# Create your tests here.
+from . import views
+
+
+class DogViewTestCase(TestCase):
+    longMessage = True    
+    def test_get(self):
+        status_code = 200
+        view_class = views.DogRandomView
+        
+        req = RequestFactory().get('/')
+        req.user = AnonymousUser()
+        resp = views.DogRandomView.as_view()(req, *[], **{})
+        self.assertEqual(resp.status_code, 200)
