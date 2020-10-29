5ff20f2080f0417032f611ccbf13f88d6683ffc3
tafernaberry <gonzalo.tafernaberry@gmail.com>
2020-10-29 18:51:06 -0300

    change in docker file

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,4 @@
-FROM python:3
-ENV PYTHONUNBUFFERED=1
-RUN mkdir /code
-WORKDIR /code
-COPY requirements.txt /code/
-RUN pip install -r requirements.txt
-COPY . /code/
-
+FROM django
+WORKDIR /usr/src/app
+CMD [ "django-admin", "startproject pythonChallenge" ]
+CMD [ "python", "manage.py runserver 0.0.0.0:8000" ]
--- pythonChallenge/urls.py
+++ pythonChallenge/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from animalsProject.views import DogRandomView, CatRandomView
+from dogs.views import DogRandomView
+from cats.views import CatRandomView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dog/random', DogRandomView.as_view(), name="home"),
-    path('cat/random', CatRandomView.as_view(), name="home"),
+    path('dog/random', DogRandomView.as_view(), name="dogs-random"),
+    path('cat/random', CatRandomView.as_view(), name="cats-random"),
 ]
